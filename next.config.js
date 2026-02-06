455c5b8ad35ef1a3a6046fe4925a3ea0f02de094
syogana <34249517+syogana@users.noreply.github.com>
2026-02-05 20:53:23 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: '/prabhavali',
+  assetPrefix: '/prabhavali',
   images: {
     unoptimized: true,
   },
